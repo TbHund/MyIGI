28e54dcf83c9f5e5dcb3862a9baa93bac27527a6
tima <timofeibogutski@gmail.com>
2025-05-27 20:21:37 +0300

    НОВОЕ НАЧАЛО

--- IGI/LR5/Hotel/Hotel/asgi.py
+++ IGI/LR5/Hotel/Hotel/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Laba5 project.
+ASGI config for Hotel project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Laba5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hotel.settings')
 
 application = get_asgi_application()
--- IGI/LR5/Hotel/Hotel/wsgi.py
+++ IGI/LR5/Hotel/Hotel/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Laba5 project.
+WSGI config for Hotel project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Laba5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hotel.settings')
 
 application = get_wsgi_application()
